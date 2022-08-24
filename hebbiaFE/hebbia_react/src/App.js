9d6e14c66b244a15329f43e63ecd471a31a3c4da
j10sanders <jonsandersss@gmail.com>
2022-08-24 16:24:23 -0400

    box

--- hebbiaFE/hebbia_react/src/App.js
+++ hebbiaFE/hebbia_react/src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Input } from "@chakra-ui/react";
+import { Input, Box } from "@chakra-ui/react";
 
 function App() {
   const [success, setSuccess] = useState(false);
@@ -43,21 +43,37 @@ function App() {
 
   return (
     <ChakraProvider>
-      <header className="App-header">
+      <Box
+        maxW="sm"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        p="6"
+      >
         <p>The best result is: {response}</p>
-        <form onSubmit={onSubmit}>
-          <label htmlFor="name">File Name:</label>
-          <Input type="text" name="name" {...register("name")} />
-          <label htmlFor="content">Content:</label>
-          <Input type="textArea" name="content" {...register("content")} />
-          <Input type="submit" value="Submit" />
-        </form>
-        <form onSubmit={onSearch}>
-          <label htmlFor="search">Search sentence:</label>
-          <Input type="textArea" name="search" {...register("search")} />
-          <Input type="submit" value="Submit" />
-        </form>
-      </header>
+        <Box p="6">
+          <Box display="flex" alignItems="baseline" pb="12">
+            <form onSubmit={onSubmit}>
+              <label htmlFor="name">File Name:</label>
+              <Input type="text" name="name" {...register("name")} />
+              <label htmlFor="content">Content:</label>
+              <Input
+                type="textArea"
+                name="content"
+                {...register("content")}
+                lg
+              />
+              <Input type="submit" value="Submit" />
+            </form>
+          </Box>
+
+          <form onSubmit={onSearch}>
+            <label htmlFor="search">Search sentence:</label>
+            <Input type="textArea" name="search" {...register("search")} />
+            <Input type="submit" value="Submit" />
+          </form>
+        </Box>
+      </Box>
     </ChakraProvider>
   );
 }
