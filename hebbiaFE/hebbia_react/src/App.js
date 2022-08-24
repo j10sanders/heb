5b1e32ab005ca5437a79fe049d86963f17789558
j10sanders <jonsandersss@gmail.com>
2022-08-24 16:54:02 -0400

    bit better

--- hebbiaFE/hebbia_react/src/App.js
+++ hebbiaFE/hebbia_react/src/App.js
@@ -2,7 +2,16 @@ import { useState } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Input, Box, Button, Container, Textarea } from "@chakra-ui/react";
+import {
+  Input,
+  Box,
+  Button,
+  Container,
+  Textarea,
+  Text,
+  Center,
+  Divider,
+} from "@chakra-ui/react";
 
 function App() {
   const [success, setSuccess] = useState(false);
@@ -43,48 +52,79 @@ function App() {
 
   return (
     <ChakraProvider>
-      <Box
-        maxW="sm"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        p="6"
-        bgGradient="linear(to-r, blue.100, blue.200)"
-      >
-        <Container centerContent>The best result is: {response}</Container>
-        <Box p="6">
-          <Box display="flex" alignItems="baseline" pb="12">
-            <form onSubmit={onSubmit}>
-              <label htmlFor="name">File Name:</label>
-              <Input type="text" name="name" {...register("name")} bg="white" />
-              <label htmlFor="content">Content:</label>
-              <Textarea
-                type="textArea"
-                name="content"
-                {...register("content")}
-                lg
-                bg="white"
-              />
-              <Button type="submit" value="Submit" mt="3">
-                Submit
-              </Button>
-            </form>
+      <Center>
+        <Box
+          maxW="sm"
+          borderWidth="1px"
+          borderRadius="lg"
+          p="6"
+          mt="20"
+          bgGradient="linear(to-r, blue.100, blue.200)"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box p="6">
+            <Text fontSize="1xl" fontWeight="bold">
+              Upload a file (as a json array):
+            </Text>
+            <Box display="flex" alignItems="baseline" pb="6">
+              <form onSubmit={onSubmit}>
+                <label htmlFor="name">File Name:</label>
+                <Input
+                  type="text"
+                  name="name"
+                  {...register("name")}
+                  bg="white"
+                />
+                <label htmlFor="content">Content:</label>
+                <Textarea
+                  type="textArea"
+                  name="content"
+                  {...register("content")}
+                  lg
+                  bg="white"
+                />
+                <Button type="submit" value="Submit" mt="3">
+                  Submit
+                </Button>
+              </form>
+            </Box>
+            <Divider />
+            <Box pt="6">
+              <form onSubmit={onSearch}>
+                <label htmlFor="search">Search sentence:</label>
+                <Input
+                  type="textArea"
+                  name="search"
+                  {...register("search")}
+                  bg="white"
+                />
+                <Button type="submit" value="Submit" mt="3">
+                  Search
+                </Button>
+              </form>
+            </Box>
           </Box>
-
-          <form onSubmit={onSearch}>
-            <label htmlFor="search">Search sentence:</label>
-            <Input
-              type="textArea"
-              name="search"
-              {...register("search")}
-              bg="white"
-            />
-            <Button type="submit" value="Submit" mt="3">
-              Search
-            </Button>
-          </form>
         </Box>
-      </Box>
+        <Box
+          minW="sm"
+          borderWidth="1px"
+          borderRadius="lg"
+          minH="400"
+          p="6"
+          mt="20"
+          bgGradient="linear(to-r, blue.100, blue.200)"
+          display="flex"
+          flexDirection="column"
+        >
+          <Container>
+            <Text fontSize="2xl">The best result is: </Text>
+            <Container centerContent bg="white" borderRadius="2">
+              {response}
+            </Container>
+          </Container>
+        </Box>
+      </Center>
     </ChakraProvider>
   );
 }
